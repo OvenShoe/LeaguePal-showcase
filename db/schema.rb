06e8e592ab93180343594e5a042cea0c45a560fc
Nathan Garcia <chilletunes@gmail.com>
2026-02-28 12:53:59 +1100

    Added games column to competition and two fields to stats

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_27_074419) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_28_010615) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -77,8 +77,10 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_27_074419) do
   create_table "stats", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.bigint "game_id", null: false
+    t.string "label"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.integer "value"
     t.index ["game_id"], name: "index_stats_on_game_id"
     t.index ["user_id"], name: "index_stats_on_user_id"
   end
