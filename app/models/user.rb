a0037fa54455cf39628ce8d13fddd799f2d99d6d
Dylan Zapirain <dylan.zapirain@gmail.com>
2026-03-21 09:08:10 +1100

    front end chnages to devise log in and sign up, form partial changed to match styling changes, admin landing page updated and create a new comp styling updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,10 @@ class User < ApplicationRecord
   has_many :stats, dependent: :destroy
   has_many :trophies, through: :team_members
 
+  def admin?
+  CompetitionAdmin.exists?(user_id: id)
+  end
+
   def is_admin?
     competition_admins.exists?
   end
